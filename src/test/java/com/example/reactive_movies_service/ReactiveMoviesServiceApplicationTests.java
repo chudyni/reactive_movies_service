7c4014bb19020dea490ccab2cd962e369780ea06
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2018-04-02 19:20:10 +0200

    Init commit

---
+++ src/test/java/com/example/reactive_movies_service/ReactiveMoviesServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.reactive_movies_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveMoviesServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
