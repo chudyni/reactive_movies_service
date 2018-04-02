5dbdf52f6fa9bb954bf4800160874da2470b7f9d
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2018-04-02 19:54:12 +0200

    Load initiald data to DB

--- src/main/java/com/example/reactive_movies_service/ReactiveMoviesServiceApplication.java
+++ src/main/java/com/example/reactive_movies_service/ReactiveMoviesServiceApplication.java
@@ -1,21 +1,31 @@
 package com.example.reactive_movies_service;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
-import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @SpringBootApplication
 public class ReactiveMoviesServiceApplication {
+
+    @Bean
+    ApplicationRunner demoData(MovieRepository movieRepository) {
+        return args -> {
+            movieRepository.deleteAll().thenMany(
+                    Flux.just("American Beauty", "Amelia", "Forest Gump", "Exorcism of Emily Rose", "Ghostbusters", "Liar, Liar")
+                            .map(Movie::new)
+                            .flatMap(movieRepository::save))
+                    .thenMany(movieRepository.findAll())
+                    .subscribe(System.out::println);
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveMoviesServiceApplication.class, args);
@@ -44,6 +54,6 @@ class Movie {
 	@Id
 	private String id;
 
-	@NotNull
+	@NonNull
 	private String title;
 }
